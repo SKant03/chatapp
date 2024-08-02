617f9ed89fa19464dab734b0da08bcd4d96fbda9
shashikant <shashikant040203@gmail.com>
2024-08-02 19:50:31 +0530

    login page frontend

--- chatapp-frontend/src/App.jsx
+++ chatapp-frontend/src/App.jsx
@@ -7,43 +7,27 @@ function App() {
 
   return (
     <>
-    <div className="bg-neutral-800 text-white font-serif">
+      <div className="signin bg-blue-500" id="signin">
+        <div className="max-w-lg min-w-96 p-5 rounded border border-white m-1 shadow-lg">
+          <form className='m-6' id='formcom'>
+            <div className='m-3 font-bold text-4xl mx-auto flex justify-center'>
+              <span className='p-2 rounded-2xl'><h2>Login</h2></span>
+            </div>
+            <div className='m-3 grid'>
+              <label className='font-bold text-2xl'>Email</label>
+              <input type="email" className='rounded-md p-2' id='Entered_email' placeholder='email' />
+            </div>
+            <div className='m-3 grid'>
+              <label className="font-bold text-2xl">Password</label>
+              <input type="password" className="rounded-md p-2" id='Entered_passward' placeholder='password' />
+            </div>
+            <div className="m-3 flex justify-center">
+              <input type="submit" className="bg-orange-400 p-2 rounded-full text-xl font-bold" id='submit_button' />
+            </div>
+          </form>
 
-
-
-    <div className="text-center font-bold text-4xl pt-5">Broadcast Chat</div>
-
-    <hr />
-
-    <div className=" flex w-screen h-screen p-5">
-
-
-      <div className="container basis-2/5 bg-neutral-700 rounded-2xl p-3">
-        <div  className="flex justify-between">
-          <div className="font-bold text-xl">Chats</div>
-          <button><FontAwesomeIcon icon="fa-brands fa-rocketchat" style={{color: "#ffffff"}}/></button>
-        </div>
-        <div className="chatsaccounts">
-          {/* the script generate the chat user */}
-          <div className='conatiner px-4 py-4 flex justify-between'>
-            <img src="" alt="userimage" />
-            <div className="font-bold">NAME</div>
-          </div>
         </div>
       </div>
-      <div className="container ">
-        <div className='conatiner px-4 py-4 flex '>
-            <img src="" alt="userimage" />
-            <div className="font-bold">NAME</div>
-          </div>
-          <div className="chat "></div>
-          <div className="container fixed-bottom">chat</div>
-      </div>
-    </div>
-
-
-
-    </div>
     </>
   )
 }
---
+++ chatapp-frontend/src/assets/components/Chat.jsx
@@ -0,0 +1,51 @@
+import { Component, useState } from 'react'
+//import './App.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="bg-neutral-800 text-white font-serif">
+
+
+
+    <div className="text-center font-bold text-4xl pt-5">Broadcast Chat</div>
+
+    <hr />
+
+    <div className=" flex w-screen h-screen p-5">
+
+
+      <div className="container basis-2/5 bg-neutral-700 rounded-2xl p-3">
+        <div  className="flex justify-between">
+          <div className="font-bold text-xl">Chats</div>
+          <button><FontAwesomeIcon icon="fa-brands fa-rocketchat" style={{color: "#ffffff"}}/></button>
+        </div>
+        <div className="chatsaccounts">
+          {/* the script generate the chat user */}
+          <div className='conatiner px-4 py-4 flex justify-between'>
+            <img src="" alt="userimage" />
+            <div className="font-bold">NAME</div>
+          </div>
+        </div>
+      </div>
+      <div className="container ">
+        <div className='conatiner px-4 py-4 flex '>
+            <img src="" alt="userimage" />
+            <div className="font-bold">NAME</div>
+          </div>
+          <div className="chat "></div>
+          <div className="container fixed-bottom">chat</div>
+      </div>
+    </div>
+
+
+
+    </div>
+    </>
+  )
+}
+
+export default App
